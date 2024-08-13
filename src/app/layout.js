1f9f2dc9d95b3c99835319859efac93fcd3fe6e3
chadley antonels <chadleycantonels@gmail.com>
2024-08-13 11:34:05 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar";
 import Footer from "./components/footer";
+import { Analytics } from "@vercel/analytics/react"
 
 const inter = Inter({ subsets: ["latin"] });
 
